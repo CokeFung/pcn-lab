acbdfcc60e74d4f164b96d16dfa271d61e7ba1ed
Fungkiat <naruebetcm@gmail.com>
2020-03-26 22:41:11 +0700

    save info to file by Frame Purinut

--- getInfo.py
+++ getInfo.py
@@ -1,63 +1,58 @@
 import telnetlib
 
-routers_list = {}
-
 devices = [
-	"100.0.10.1",
-	"100.0.20.1",
-	"100.0.30.1",
-	"100.0.40.1",
-	"100.0.50.1",
-	"100.0.60.2"
-	]
+    "192.168.0.1",
+    "192.168.7.2",
+    "192.168.8.2",
+    "192.168.9.2",
+    "192.168.10.2",
+    "192.168.11.2"
+    ]
 user = "cisco"
-passwd = "cisco"
-en_pass = "class"
+password = "cisco"
+en_pass = "cisco"
 
-for device in devices: 
+for device in devices:
+    #connect
+    tn = telnetlib.Telnet(device)
 
-  #connect
-  #print("Connecting to", device)
-  tn = telnetlib.Telnet(device)
+    #authen
+    tn.read_until(b"Password: ").decode('ascii')
+    tn.write(password.encode('ascii') + b"\n")
 
-  #authen
-  tn.read_until(b"Password: ").decode('ascii')
-  tn.write(passwd.encode('ascii') + b"\n")
+    #acess privilege
+    tn.read_until(b">")
+    tn.write(b"\n")
+    router_name = tn.read_until(b">").decode('ascii').replace('\r', '').replace('\n', '')[:-1]
+    tn.write(b"enable\n")
+    tn.read_until(b"Password: ").decode('ascii')
+    tn.write(en_pass.encode('ascii') + b"\n")
+    tn.read_until(b"#").decode('ascii')
+    tn.write(b"terminal length 0\n")
+    tn.read_until(b"#").decode('ascii')
 
-  #access privilege mode and get router name
-  router_name = tn.read_until(b">").decode('ascii').replace('\r', '').replace('\n', '')[:-1]
-  tn.write(b"enable\n")
-  tn.read_until(b"Password: ").decode('ascii')
-  tn.write(en_pass.encode('ascii') + b"\n")
-  tn.read_until(b"#").decode('ascii')
-  tn.write(b"terminal length 0\n")
-  tn.read_until(b'#').decode('ascii')
+    #sending command
+    tn.write(b"show version\n")
+    tn.read_until(b"show version")
+    router_spec = tn.read_until(b"#").decode('ascii').replace('\r', '')[1:-3]
 
-  #send command and get output  (specifications)
-  tn.write(b"show version\n")
-  tn.read_until(b'show version')
-  router_spec = tn.read_until(b'#').decode('ascii').replace('\r', '')[1:-3]
-  #print(router_spec, end='')
+    tn.write(b"show ip int brief\n")
+    tn.read_until(b"brief")
+    router_int = tn.read_until(b"#").decode('ascii').replace('\r', '')[1:-3]
 
+    tn.write(b"show ip route\n")
+    tn.read_until(b"show ip route")
+    router_table = tn.read_until(b"#").decode('ascii').replace('\r', '')[1:-3]
 
-  #send command and get output  (interfaces)
-  tn.write(b"show ip int brief\n")
-  tn.read_until(b'brief')
-  router_int = tn.read_until(b'#').decode('ascii').replace('\r', '')[1:-3]
+    #disconnect
+    tn.write(b"exit\n")
 
-  #send command and get output
-  tn.write(b"show ip route\n")
-  tn.read_until(b"show ip route")
-  router_route = tn.read_until(b'#').decode('ascii').replace('\r', '')[1:-3]
+    print(tn.read_all().decode('ascii'))
+    content = "Name: " + router_name + "\nSpec: \n" + router_spec + \
+    "\nInterface: \n" + router_int + "\nRouting table: \n" + router_table + "\n"
 
-  #disconnect
-  tn.write(b"exit\n")
-
-  print(tn.read_all().decode('ascii'))
-  print("Name: " + router_name)
-  print("Spec: \n" + router_spec)
-  print("Interface: \n" + router_int)
-  print("Routing Table: \n" + router_route)
-  print('\n')
-
-
+    file_name = router_name + ".txt"
+    save_out = open(file_name, "w")
+    save_out.write(content + "\n")
+    save_out.close()
+    
